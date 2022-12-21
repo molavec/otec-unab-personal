03c52b3a616800a8a2ed58797166e89ff6e9c8d5
Miguel Olave <molavec@gmail.com>
2022-12-20 21:55:16 -0300

    jquery

--- M2-ABP-Portafolio-Github-JS/js/index.js
+++ M2-ABP-Portafolio-Github-JS/js/index.js
@@ -1,5 +1,5 @@
 
 const date = new Date();
-//const dateString = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
-console.log(dateString)
+const dateString = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+//console.log(dateString)
 document.getElementById('date').innerHTML=dateString;
--- M2-ABP-Portafolio-jquery/js/index.js
+++ M2-ABP-Portafolio-jquery/js/index.js
@@ -1,5 +1,6 @@
 
 const date = new Date();
-//const dateString = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
-console.log(dateString)
-document.getElementById('date').innerHTML=dateString;
+const dateString = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+//console.log(dateString);
+
+$('date').html(dateString);
