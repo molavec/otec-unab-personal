129f21bf48744cfcf2845a5580986bdd7161fde6
Miguel Olave <molavec@gmail.com>
2022-12-20 21:51:11 -0300

    cambia nombre

---
+++ M2-ABP-Portafolio-jquery/js/index.js
@@ -0,0 +1,5 @@
+
+const date = new Date();
+//const dateString = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+console.log(dateString)
+document.getElementById('date').innerHTML=dateString;
